083d59d0e82cc12e7ba93a0b21230977b36143bf
truuue <noahvernhet@gmail.com>
2023-08-22 14:42:02 -0700

    Revert all changes + add chmod

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,12 +2,12 @@
 """Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress"""
 
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-
+    
     employee_id = int(sys.argv[1])
 
     todos_response = requests.get(
@@ -27,14 +27,14 @@ if __name__ == "__main__":
             break
 
     for task in todos:
-        if task["userId"] == employee_id and task["completed"]:
-            employee_completed_tasks.append(task["title"])
+        if task["userId"] == employee_id:
+            if task["completed"]:
+                employee_completed_tasks.append(task["title"])
 
-    total_tasks = len(
-        [task for task in todos if task["userId"] == employee_id])
+    total_tasks = sum(1 for task in todos if task["userId"] == employee_id)
     num_completed_tasks = len(employee_completed_tasks)
 
     print(f"Employee {employee_name} is done with tasks({
-        len(num_completed_tasks)}/{len(total_tasks)}):")
-    for completed_task in employee_completed_tasks:
-        print("\t ", completed_task)
+        num_completed_tasks}/{total_tasks}):")
+    for title in employee_completed_tasks:
+        print("\t ", title)
