41caec061c6d2fb1e8bb02ecac2ef67467357513
truuue <noahvernhet@gmail.com>
2023-08-22 05:39:41 -0700

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    
+    employee_id = int(sys.argv[1])
+
+    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    employees_response = requests.get(
+        "https://jsonplaceholder.typicode.com/users")
+
+    todos = todos_response.json()
+    employees = employees_response.json()
+
+    employee_name = None
+    employee_completed_tasks = []
+
+    for employee in employees:
+        if employee["id"] == employee_id:
+            employee_name = employee["name"]
+            break
+
+    for task in todos:
+        if task["userId"] == employee_id:
+            if task["completed"]:
+                employee_completed_tasks.append(task["title"])
+
+    total_tasks = sum(1 for task in todos if task["userId"] == employee_id)
+    num_completed_tasks = len(employee_completed_tasks)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_completed_tasks, total_tasks))
+    for title in employee_completed_tasks:
+        print("\t ", title)
