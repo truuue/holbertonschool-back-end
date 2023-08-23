d179ceae200ff541aea79796bb3388afd90fe3dd
truuue <noahvernhet@gmail.com>
2023-08-23 04:36:18 -0700

    Export to JSON 3

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -19,24 +19,24 @@ if __name__ == "__main__":
 
     employee_name = None
 
-    for employee in employees:
-        if employee["id"] == employee_id:
-            employee_name = employee["username"]
-            break
-
+    employee_id = int(sys.argv[1])
+    json_object = {}
     tasks_list = []
+
+    for employee in employees:
+        if employee.get("id") == employee_id:
+            our_employee = employee
 
     for task in todos:
         if task.get("userId") == employee_id:
-            task_dict = {
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": employee_name
-            }
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = our_employee.get("username")
             tasks_list.append(task_dict)
 
-    json_object = {str(employee_id): tasks_list}
-    filename = str(employee_id) + ".json"
+    json_object[str(our_employee.get("id"))] = tasks_list
+    filename = str(our_employee.get("id")) + ".json"
 
     with open(filename, "w") as file:
-        json.dump(json_object, file, indent=4)
+        json.dump(json_object, file)
