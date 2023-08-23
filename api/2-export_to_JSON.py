b59b40e65c13b2663931a3fde617abdc2b4a3dbb
truuue <noahvernhet@gmail.com>
2023-08-23 04:39:32 -0700

    Export to JSON 4

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,8 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-
     todos_response = requests.get(
         "https://jsonplaceholder.typicode.com/todos")
     employees_response = requests.get(
@@ -17,26 +15,23 @@ if __name__ == "__main__":
     todos = todos_response.json()
     employees = employees_response.json()
 
-    employee_name = None
-
     employee_id = int(sys.argv[1])
     json_object = {}
     tasks_list = []
 
     for employee in employees:
         if employee.get("id") == employee_id:
-            our_employee = employee
+            employs = employee
 
     for task in todos:
         if task.get("userId") == employee_id:
             task_dict = {}
             task_dict["task"] = task.get("title")
             task_dict["completed"] = task.get("completed")
-            task_dict["username"] = our_employee.get("username")
+            task_dict["username"] = employs.get("username")
             tasks_list.append(task_dict)
 
-    json_object[str(our_employee.get("id"))] = tasks_list
-    filename = str(our_employee.get("id")) + ".json"
-
+    json_object[str(employs.get("id"))] = tasks_list
+    filename = str(employs.get("id")) + ".json"
     with open(filename, "w") as file:
         json.dump(json_object, file)
